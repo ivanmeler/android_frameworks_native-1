CyanogenMod/android_frameworks_native

exynos4: getphys implementation on buffermanager for samsung omx
Change-Id: Iee665f56aaf601479d796703db983ca5fce9c5c3

Conflicts:
	include/media/hardware/HardwareAPI.h
	include/ui/GraphicBufferMapper.h
	libs/ui/GraphicBufferMapper.cpp

include/ui/GraphicBufferMapper.h
@@ -50,6 +50,10 @@ public:
 
     status_t unlock(buffer_handle_t handle);
     
+#ifdef EXYNOS4210_ENHANCEMENTS
+    status_t getphys(buffer_handle_t handle, void** paddr);
+#endif
+
     // dumps information about the mapping of this handle
     void dump(buffer_handle_t handle);
 

libs/ui/GraphicBufferMapper.cpp
@@ -109,5 +109,18 @@ status_t GraphicBufferMapper::unlock(buffer_handle_t handle)
     return err;
 }
 
+#ifdef EXYNOS4210_ENHANCEMENTS
+status_t GraphicBufferMapper::getphys(buffer_handle_t handle, void** paddr)
+{
+    status_t err;
+
+    err = mAllocMod->getphys(mAllocMod, handle, paddr);
+
+    ALOGW_IF(err, "getphys(%p) fail %d(%s)",
+    handle, err, strerror(-err));
+    return err;
+}
+#endif
+
 // ---------------------------------------------------------------------------
 }; // namespace android

ivanmeler/android_frameworks_native-1

Change-Id: I88db141e29a50986bee8d9555d145d1f92ea6ecf

include/ui/GraphicBufferMapper.h
@@ -58,6 +58,10 @@ public:
 
     status_t unlockAsync(buffer_handle_t handle, int *fenceFd);
     
+#ifdef EXYNOS4_ENHANCEMENTS
+    status_t getphys(buffer_handle_t handle, void** paddr);
+#endif
+
     // dumps information about the mapping of this handle
     void dump(buffer_handle_t handle);
 

libs/ui/GraphicBufferMapper.cpp
@@ -178,5 +178,19 @@ status_t GraphicBufferMapper::unlockAsync(buffer_handle_t handle, int *fenceFd)
     return err;
 }
 
+#ifdef EXYNOS4_ENHANCEMENTS
+status_t GraphicBufferMapper::getphys(buffer_handle_t handle, void** paddr)
+{
+    status_t err;
+
+    err = mAllocMod->getphys(mAllocMod, handle, paddr);
+
+    ALOGW_IF(err, "getphys(%p) fail %d(%s)",
+    handle, err, strerror(-err));
+    return err;
+}
+#endif
+
+
 // ---------------------------------------------------------------------------
 }; // namespace android